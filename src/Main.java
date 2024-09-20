69b81a2e127f9f29819b20c59a3b7f4fd7e3d463
Nan Ye <ynyz2003@gmail.com>
2024-09-20 01:01:01 -0500

    problem 1 added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        int[] a = {2,7,11,15};
+        Solution1 s = new Solution1();
+
+        int[] result = s.twoSum(a, 9);
+        System.out.println(result[0]+", "+result[1]);
 
     }
 }
