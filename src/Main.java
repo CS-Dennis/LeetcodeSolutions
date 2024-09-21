7e95114c28fc37a49c52d9e2bcd6dd0aafd57004
Nan Ye <ynyz2003@gmail.com>
2024-09-20 23:20:56 -0500

    problem 2 added

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,29 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = {2,7,11,15};
-        Solution1 s = new Solution1();
+//        int[] a= {2,4,3};
+//        int[] b= {5,6,4};
+        ListNode a = new ListNode(3);
+        a.next = new ListNode(7);
+//        a.next.next = new ListNode(9);
+//        a.next.next.next = new ListNode(9);
+//        a.next.next.next.next = new ListNode(9);
+//        a.next.next.next.next.next = new ListNode(9);
+//        a.next.next.next.next.next.next = new ListNode(9);
 
-        int[] result = s.twoSum(a, 9);
-        System.out.println(result[0]+", "+result[1]);
+        ListNode b = new ListNode(9);
+        b.next = new ListNode(2);
+//        b.next.next = new ListNode(4);
+//        b.next.next.next = new ListNode(9);
+//        b.next.next.next.next = new ListNode(9);
+//        b.next.next.next.next.next = new ListNode(9);
+//        b.next.next.next.next.next.next = new ListNode(9);
+//        b.next.next.next.next.next.next.next = new ListNode(9);
+//        b.next.next.next.next.next.next.next.next = new ListNode(9);
+//        b.next.next.next.next.next.next.next.next.next = new ListNode(9);
+
+        Solution2 s = new Solution2();
+        s.addTwoNumbers(a,b);
 
     }
 }
