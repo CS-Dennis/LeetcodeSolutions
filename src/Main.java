c1892933fa3f4ba9f8669567c6cfc606d91a1dd1
Nan Ye <ynyz2003@gmail.com>
2024-09-21 21:14:42 -0500

    problem 3 added

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] a= {2,4,3};
-//        int[] b= {5,6,4};
-        ListNode a = new ListNode(3);
-        a.next = new ListNode(7);
-//        a.next.next = new ListNode(9);
-//        a.next.next.next = new ListNode(9);
-//        a.next.next.next.next = new ListNode(9);
-//        a.next.next.next.next.next = new ListNode(9);
-//        a.next.next.next.next.next.next = new ListNode(9);
-
-        ListNode b = new ListNode(9);
-        b.next = new ListNode(2);
-//        b.next.next = new ListNode(4);
-//        b.next.next.next = new ListNode(9);
-//        b.next.next.next.next = new ListNode(9);
-//        b.next.next.next.next.next = new ListNode(9);
-//        b.next.next.next.next.next.next = new ListNode(9);
-//        b.next.next.next.next.next.next.next = new ListNode(9);
-//        b.next.next.next.next.next.next.next.next = new ListNode(9);
-//        b.next.next.next.next.next.next.next.next.next = new ListNode(9);
-
-        Solution2 s = new Solution2();
-        s.addTwoNumbers(a,b);
+        String a = "abcabcbb";
+        Solution3 s = new Solution3();
+        System.out.println(s.lengthOfLongestSubstring(a));
 
     }
 }
