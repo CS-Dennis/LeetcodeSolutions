6a1178d97620d1ef112ed7a5303e80ae2a10e856
Nan Ye <ynyz2003@gmail.com>
2024-09-28 17:23:50 -0500

    problem 16 added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] a  = {34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49};
-        Solution15 s = new Solution15();
-        System.out.println(s.threeSum(a));
+        int[] a  = {4,0,5,-5,3,3,0,-4,-5};
+
+        Solution16 s = new Solution16();
+        System.out.println(s.threeSumClosest(a, -2));
     }
 }
