e6554d1add3d8e98b21e23b1a29e7ea9eb941dbd
Nan Ye <ynyz2003@gmail.com>
2024-10-03 23:14:59 -0500

    problem 30 added

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ListNode a = new ListNode(1);
-        a.next = new ListNode(2);
-        a.next.next = new ListNode(3);
-        a.next.next.next = new ListNode(4);
-        a.next.next.next.next = new ListNode(5);
-        Solution19 s = new Solution19();
-        ListNode result = s.removeNthFromEnd(a, 2);
-        while (result.next!=null){
-            System.out.println(result.val);
-            result = result.next;
-        }
-        System.out.println(result.val);
+        String s = "barfoothefoobarman";
+        String[] words = {"foo","bar"};
+        Solution30 abc = new Solution30();
+        System.out.println(abc.findSubstring(s, words));
     }
 }
---
+++ src/Solution30.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Solution30 {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+
+        int sLength = s.length();
+        int wordsLength = words.length;
+        int wordLength = words[0].length();
+        int subStringLength = wordLength * wordsLength;
+
+        if (sLength == 0 || wordsLength == 0) {
+            return result;
+        }
+
+        Map<String, Integer> wordFrequency = new HashMap<>();
+        for (String word : words) {
+            wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+        }
+
+        if (wordFrequency.keySet().size() == 1) {
+            StringBuilder sb = new StringBuilder();
+            for (String w : words) {
+                sb.append(w);
+            }
+
+            for (int i = 0; i <= s.length() - subStringLength; i++) {
+                if (s.substring(i, i + subStringLength).equals(sb.toString())) {
+                    result.add(i);
+                }
+            }
+
+            return result;
+        }
+
+//        shift only by the word length
+        for (int i = 0; i < wordLength; i++) {
+            Map<String, Integer> wordFrequencyCopy = wordFrequency;
+
+//            chop the string in words with wordLength of each and compare to the words map
+            for (int j = i; j <= sLength - subStringLength; j += wordLength) {
+//                get the sub string based on the total subStringLength
+                String currentSubString = s.substring(j, j + subStringLength);
+//                reset wordFrequencyCopy
+                wordFrequencyCopy = new HashMap<>(wordFrequency);
+                int kRecorder = 0;
+                boolean matched = true;
+//                chop the sub string into words that each has the wordLength
+                for (int k = 0; k <= subStringLength - wordLength; k += wordLength) {
+                    if (wordFrequencyCopy.containsKey(currentSubString.substring(k, k + wordLength))) {
+                        wordFrequencyCopy.put(currentSubString.substring(k, k + wordLength), wordFrequencyCopy.get(currentSubString.substring(k, k + wordLength)) - 1);
+                    } else {
+                        kRecorder = k;
+                        matched = false;
+                        break;
+                    }
+                }
+
+                if (matched) {
+//                    check wordFrequencyCopy and all key's values are 0
+                    boolean allZeroed = true;
+                    for (String key : wordFrequencyCopy.keySet()) {
+                        if (wordFrequencyCopy.get(key) != 0) {
+                            allZeroed = false;
+//                            j = j+subStringLength-wordLength;
+                            break;
+                        }
+                    }
+
+                    if (allZeroed) {
+                        result.add(j);
+                    }
+
+                }
+//                if not matched, start from the word right behind the last non-matching word
+                else {
+                    j = kRecorder + j;
+                }
+            }
+        }
+
+        return result;
+    }
+}
