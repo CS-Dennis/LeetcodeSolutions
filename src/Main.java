67c9646ccaaa9a869da194dc3354cb2c3e4180b9
Nan Ye <ynyz2003@gmail.com>
2024-10-06 17:52:35 -0500

    problem 32 added

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "barfoothefoobarman";
-        String[] words = {"foo","bar"};
-        Solution30 abc = new Solution30();
-        System.out.println(abc.findSubstring(s, words));
+        String a = "))))()()))))";
+        Solution32 s = new Solution32();
+        System.out.println(s.longestValidParentheses(a));
     }
 }
---
+++ src/Solution32.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class Solution32 {
+    public int longestValidParentheses(String s) {
+        int maxLength = 0;
+
+//        stack approch
+        Stack<Integer> stack = new Stack<>();
+        stack.add(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='('){
+                stack.add(i);
+            } else {
+                stack.pop();
+
+                if(stack.isEmpty()){
+                    stack.add(i);
+                } else {
+                    if(maxLength< i-stack.peek()){
+                        maxLength = i-stack.peek();
+                    }
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
