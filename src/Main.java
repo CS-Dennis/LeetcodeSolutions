6f4aad5224e61819514543baa4d9390982019829
Nan Ye <ynyz2003@gmail.com>
2024-10-19 16:24:27 -0500

    problem 46 added

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums ={2,3,1,1,4};
-        Solution45 s = new Solution45();
-        System.out.println(s.jump(nums));
+        int[] nums ={1,2,3};
+        Solution46 s = new Solution46();
+        List<Integer> a = new ArrayList<>();
+        System.out.println(s.permute(nums));
     }
 }
---
+++ src/Solution46.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution46 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> remainingList = new ArrayList<>();
+        for (Integer i : nums) {
+            remainingList.add(i);
+        }
+
+        permutateList(result, null, remainingList, nums.length);
+
+        return result;
+    }
+
+    private void permutateList(List<List<Integer>> result, List<Integer> currentList, List<Integer> remainingList, int totalLength) {
+        if (currentList != null && currentList.size() == totalLength) {
+            result.add(currentList);
+        } else {
+            int i = remainingList.size() - 1;
+            while (i >= 0) {
+                List<Integer> tempList = new ArrayList<>();
+                if (currentList != null) {
+                    for (Integer num : currentList) {
+                        tempList.add(num);
+                    }
+                }
+
+                tempList.add(remainingList.get(i));
+                List<Integer> newRemainingList = new ArrayList<>();
+                for (Integer digit : remainingList) {
+                    newRemainingList.add(digit);
+                }
+                newRemainingList.remove(i);
+                permutateList(result, tempList, newRemainingList, totalLength);
+                i--;
+            }
+        }
+    }
+}
