8e9ec8db8264f71f30adcc222953a03841239e9c
Nan Ye <ynyz2003@hotmail.com>
2024-10-22 20:46:05 -0500

    problem 50 added

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String[] strs = {"aab","aabb"};
-        Solution49 s = new Solution49();
-
-        System.out.println(s.groupAnagrams(strs));
-//        String a="123";
-//        String b = a;
-//        b="789";
-//        System.out.println(a);
+        Solution50 s = new Solution50();
+        System.out.println(s.myPow(-2, 2));
     }
 }
---
+++ src/Solution50.java
@@ -0,0 +1,39 @@
+public class Solution50 {
+    public double myPow(double x, int n) {
+        if (x == 0) {
+            return 0;
+        } else if (x == 1) {
+            return 1;
+        } else if (x == -1) {
+            if (n % 2 == 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+        double result = 1;
+
+        int newN = n;
+        if (newN < 0) {
+            if (newN < -50) {
+//                very small decimal, just return 0 for double
+                return 0;
+            } else {
+                newN = -newN;
+            }
+        }
+        while (newN > 0) {
+            if (newN % 2 == 0) {
+//                2* 2^16 =2 * 4^8
+                x *= x;
+                newN = newN / 2;
+            } else {
+//                2^17 = 2 * 2^16
+                result *= x;
+                newN--;
+            }
+        }
+        return n < 0 ? 1 / result : result;
+    }
+}
