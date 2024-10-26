4a6332dcd6c246d599d9aca45b53d1445b83224f
Nan Ye <ynyz2003@hotmail.com>
2024-10-26 15:40:29 -0500

    problem 57 added

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import SolvedProblems.Solution54;
 public class Main {
     public static void main(String[] args) {
 //        int[][] nums = {{2,3},{4,5},{6,7},{1,10}};
-        int[][] nums = {{1,3},{4,6},{4,7}};
-        Solution56 s = new Solution56();
-        s.merge(nums);
+        int[][] nums = {{1,2},{3,5},{6,7},{8,10},{12,16}};
+        Solution57 s = new Solution57();
+        s.insert(nums, new int[]{1,2});
     }
 }
