666237f2040784914643e47ad05845d5f328d21f
Nan Ye <ynyz2003@hotmail.com>
2024-12-10 20:36:51 -0600

    Solution 63 added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import SolvedProblems.Solution62;
 
 public class Main {
     public static void main(String[] args) {
-        Solution62 s = new Solution62();
-        System.out.println(s.uniquePaths(3,7));
+        Solution63 s = new Solution63();
+        System.out.println(s.uniquePathsWithObstacles(new int[][]{{0,0}, {0,1}}));
     }
 }
---
+++ src/Solution63.java
@@ -0,0 +1,29 @@
+public class Solution63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        Integer[] array = {0};
+        findPaths(m, n, array, obstacleGrid);
+        return array[0];
+    }
+
+    void findPaths(int m, int n, Integer[] array, int[][] grid){
+
+        if(m==1 && n==1 && grid[grid.length-1][grid[0].length-1]==0){
+            array[0] += 1;
+            return;
+        }
+
+        if(m>1){
+            if(grid[m-2][n-1]==0){
+                findPaths(m-1, n, array, grid);
+            }
+        }
+
+        if(n>1){
+            if(grid[m-1][n-2]==0){
+                findPaths(m, n-1, array, grid);
+            }
+        }
+    }
+}
