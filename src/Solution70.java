22f1c2abc7585ad024d1466c78c2ccaf338f7fd1
Nan Ye <ynyz2003@hotmail.com>
2024-12-13 17:31:12 -0600

    added Solutions 70 and 71

--- src/Solution70.java
+++ src/Solution70.java
@@ -1,97 +1,16 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Solution70 {
-    public String simplifyPath(String path) {
-//        1. remove //
-        String regex = "//"; // "(\\.\\.\\.)*[a-zA-Z_]+(\\.\\.\\.)*[aA_]*/\\.\\./"
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "/");
-            matcher = pattern.matcher(path);
-        }
+    public int climbStairs(int n) {
+        Integer[] ways = new Integer[]{0};
+        findUniqueWays(n, ways);
+        return ways[0];
+    }
 
-//        2. remove /./
-        regex = "/\\./";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll("/\\./", "/");
-            matcher = pattern.matcher(path);
+    void findUniqueWays(int currentStairs, Integer[] ways) {
+        if (currentStairs <= 1) {
+            ways[0] = ways[0] + 1;
+        } else {
+            findUniqueWays(currentStairs - 1, ways);
+            findUniqueWays(currentStairs - 2, ways);
         }
-
-//      3. remove [filename]/../
-        regex = "[\\.]*[a-zA-Z_]+/\\.\\./";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "");
-            matcher = pattern.matcher(path);
-        }
-//        4. remove leading /../
-        regex = "^/\\.\\./";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "/");
-            matcher = pattern.matcher(path);
-        }
-
-//      5. remove ending [filename]/..
-        regex = "[a-zA-Z_]+/\\.\\.$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "");
-            matcher = pattern.matcher(path);
-        }
-
-        regex = "/\\.$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "");
-            matcher = pattern.matcher(path);
-        }
-
-        regex = "[a-zA-Z]*[\\.]*/\\.\\./$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "/");
-            matcher = pattern.matcher(path);
-        }
-
-        regex = "[a-zA-Z]*[\\.]*/\\.\\.$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "/");
-            matcher = pattern.matcher(path);
-        }
-
-        regex = "/$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "");
-            matcher = pattern.matcher(path);
-        }
-
-        regex = "^\\.\\.$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(path);
-        while (matcher.find()) {
-            path = path.replaceAll(regex, "");
-            matcher = pattern.matcher(path);
-        }
-
-        if (path.isEmpty() || path.charAt(0) != '/') {
-            path = "/" + path;
-        }
-
-
-        return path;
     }
 }
---
+++ src/Solution71.java
@@ -0,0 +1,97 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution71 {
+    public String simplifyPath(String path) {
+//        1. remove //
+        String regex = "//"; // "(\\.\\.\\.)*[a-zA-Z_]+(\\.\\.\\.)*[aA_]*/\\.\\./"
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "/");
+            matcher = pattern.matcher(path);
+        }
+
+//        2. remove /./
+        regex = "/\\./";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll("/\\./", "/");
+            matcher = pattern.matcher(path);
+        }
+
+//      3. remove [filename]/../
+        regex = "[.]*[a-zA-Z_]+/\\.\\./";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "");
+            matcher = pattern.matcher(path);
+        }
+//        4. remove leading /../
+        regex = "^/\\.\\./";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "/");
+            matcher = pattern.matcher(path);
+        }
+
+//      5. remove ending [filename]/..
+        regex = "[a-zA-Z_]+/\\.\\.$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "");
+            matcher = pattern.matcher(path);
+        }
+
+        regex = "/\\.$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "");
+            matcher = pattern.matcher(path);
+        }
+
+        regex = "[a-zA-Z]*[.]*/\\.\\./$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "/");
+            matcher = pattern.matcher(path);
+        }
+
+        regex = "[a-zA-Z]*[.]*/\\.\\.$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "/");
+            matcher = pattern.matcher(path);
+        }
+
+        regex = "/$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "");
+            matcher = pattern.matcher(path);
+        }
+
+        regex = "^\\.\\.$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(path);
+        while (matcher.find()) {
+            path = path.replaceAll(regex, "");
+            matcher = pattern.matcher(path);
+        }
+
+        if (path.isEmpty() || path.charAt(0) != '/') {
+            path = "/" + path;
+        }
+
+
+        return path;
+    }
+}
