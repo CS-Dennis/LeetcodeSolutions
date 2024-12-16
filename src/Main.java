638803c5ab41b070abaf0f1da1cf04df6860de59
Nan Ye <ynyz2003@hotmail.com>
2024-12-15 22:19:22 -0600

    Solution72 added

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import SolvedProblems.Solution62;
 
 public class Main {
     public static void main(String[] args) {
-        Solution70 s = new Solution70();
-        String a ="/home/foo/.ssh/../.ssh2/authorized_keys/";
-//        String a = "/..";
-        System.out.println(a);
-        System.out.println(s.simplifyPath(a));
+        Solution72 s = new Solution72();
+        System.out.println(s.minDistance("distance", "springbok"));
     }
 }
